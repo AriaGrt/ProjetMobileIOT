e2a61d8dfa1044efca7a2a9a0b078b39722e5164
Aria Groult <aria.groult@outlook.fr>
2018-11-02 15:43:31 +0100

    post working

--- client/src/components/LanguageDetector.vue
+++ client/src/components/LanguageDetector.vue
@@ -1,26 +1,50 @@
 <template>
-    <div class="ui main text container">
+    <div id="LanguageDetector" class="ui main text container">
         <div class="ui segment">
             <h1 class="ui header">Trouver la langue d'un texte</h1>
             <div class="ui form">
                 <div class="field">
                     <!--<label>Text</label>-->
-                    <textarea></textarea>
+                    <textarea v-model="input"></textarea>
                 </div>
-                <div class="ui primary large fluid button">Valider</div>
+                <div class="ui primary large fluid button" v-on:click="sendText()">Valider</div>
             </div>
         </div>
         <div class="ui segment">
-            <h2 class="ui header">Langue trouvé</h2>
-            <div class="ui segment secondary"></div>
+            <h2 class="ui header">Langue trouvée</h2>
+            <div class="ui segment secondary">{{language}}</div>
         </div>
 
     </div>
 </template>
 
 <script>
+    import axios from "axios"
+
     export default {
-        name: "LanguageDetector"
+        name: 'LanguageDetector',
+        data() {
+            return {
+                input: "",
+                language: ""
+            }
+        },
+        methods: {
+            sendText: async function () {
+                let data= '';
+                let input = this.input;
+                await axios.post('http://localhost:3000/post-text', {
+                    text: input
+                })
+                    .then(function (response) {
+                        data = response.data;
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+                this.language = data;
+            }
+        }
     }
 </script>
 
@@ -28,6 +52,7 @@
     .main {
         margin-top: 6em;
     }
+
     /* Mobile */
     @media only screen and (max-width: 767px) {
         h1 {
